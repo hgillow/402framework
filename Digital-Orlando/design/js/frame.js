f885897acb65d8ffd11b46a85dc18fda9130fbb2
hgkbergen <hannahgillow@gmail.com>
2014-04-27 10:50:59 -0500

    orlando1

---
+++ Digital-Orlando/design/js/frame.js
@@ -0,0 +1,8 @@
+/**
+ * frame.js - default js for Digital Orlando using jquery library
+ */
+ 
+//JQuery UI tooltip for document
+$(document).ready(function() { 
+  $(document).tooltip();
+});
---
+++ Digital-Orlando/plugins/scroller/scroller.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+
+
+
+
+
+var el = document.getElementById();
+el.parentNode.scrollTop = el.offsetTop - el.parentNode.offsetHeight + el.offsetHeight - 5;
+
+var el = document.getElementById();
+el.parentNode.scrollTop = el.offsetTop - el.parentNode.offsetHeight + el.offsetHeight - 5;
+
+</script>
